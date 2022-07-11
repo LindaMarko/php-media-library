6195c167b15c562baa6a730aac532f8a5611c97d
Linda Marko <lindamarko83@gmail.com>
2022-07-11 11:30:53 +0200

    Add _GET variables, active states to nav

--- catalog.php
+++ catalog.php
@@ -1,7 +1,38 @@
-<?php include('inc/header.php'); ?>
+<?php
+$catalog = array(
+	"Design Patterns",
+	"Forrest Gump",
+	"Beethoven");
 
-<div class="section page">
-  <h1>Full Catalog</h1>
+$pageTitle = 'Full Catalog';
+$section = null;
+
+if (isset($_GET["cat"])){
+  if ( $_GET["cat"] == "books") {
+    $pageTitle = "Books";
+    $section = "books";
+  }else if ( $_GET["cat"] == "movies") {
+    $pageTitle = "Movies";
+    $section = "movies";
+  }else if ( $_GET["cat"] == "music") {
+    $pageTitle = "Music";
+    $section = "music";
+  }
+}
+
+include('inc/header.php'); ?>
+
+<div class="section catalog page">
+  <div class="wrapper">
+    <h1><?php echo $pageTitle; ?></h1>
+    <ul>
+      <?php foreach($catalog as $item){
+        echo "<li>" . $item . "</li>";
+      }
+      ?>
+    </ul>
+  </div>
+
 </div>
 
 <?php include('inc/footer.php'); ?>
